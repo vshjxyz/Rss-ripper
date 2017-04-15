a7be67e6b4c5fa9e9407de6c708ae7b3dc014e5e
Luca Del Bianco <vshjxyz@gmail.com>
2017-04-15 15:02:41 +0200

    Adding certificates for roachr

--- src/constants/db.js
+++ src/constants/db.js
@@ -1,4 +1,14 @@
-export default process.env.ROACHR_URL || {
+import { parse } from 'pg-connection-string'
+import fs from 'fs'
+
+export default process.env.ROACHR_URL ? {
+  ...parse(process.env.ROACHR_URL),
+  database: 'rss_ripper_db',
+  ssl: {
+    key: fs.readFileSync('./root.key'),
+    cert: fs.readFileSync('./root.cert')
+  }
+} : {
   database: 'rss_ripper_db',
   user: 'rss_ripper_user',
   host: 'localhost',
