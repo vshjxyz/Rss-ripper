96cf080ca8afc794c5c50fb135d40b3b3efc655c
Luca Del Bianco <vshjxyz@gmail.com>
2018-07-26 18:36:38 +0200

    Fixing upsert query

--- src/streams/write.stream.js
+++ src/streams/write.stream.js
@@ -2,6 +2,6 @@ import db from '../db'
 
 export default (key, value) => {
   return db.flatMap((pool) =>
-    pool.query('UPSERT INTO news (id, value) VALUES ($1, $2)', [key, value])
+    pool.query('INSERT INTO news (id, value) VALUES ($1, $2) ON CONFLICT (id) DO UPDATE SET value=$2', [key, value])
   )
 }
